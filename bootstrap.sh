9515f6ba391fe12529d730a1853b0902205fb2c3
hieunt79 <nguyenhieu26496@gmail.com>
2025-04-09 10:44:38 +0700

    bootstrap: update

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,6 +9,7 @@ function main {
     if [ "$#" -ne 1 ]; then
         echo ""
         echo "Pls $0 dev|server [--debug]"
+        exit 1
     fi
     dev_enable=$1
 
@@ -24,24 +25,27 @@ function main {
 }
 
 function install_vim {
+    # install vim
+    echo "Installing vimrc"
     SCRIPT_PATH=$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
     DEV_ENABLE=$1
 
-    # install vim
     VIM_PATH="dotvim"
-    if [ -d "$HOME/.vim" ]; then
-        rm $HOME/.vim/vimrc
-        ln -s $SCRIPT_PATH/$VIM_PATH/vimrc $HOME/.vim/vimrc
-        #
-        if [[ $DEV_ENABLE == "true" ]]; then
-            rm $HOME/.vim/plugins.vim
-            ln -s $SCRIPT_PATH/$VIM_PATH/plugins.vim $HOME/.vim/plugins.vim
-            #
-            rm -f $HOME/.vim/syntax.old
-            mv $HOME/.vim/syntax $HOME/.vim/syntax.old
-            ln -s $SCRIPT_PATH/$VIM_PATH/syntax $HOME/.vim/syntax
-        fi
+    if [ ! -d "$HOME/.vim" ]; then
+        mkdir -p $HOME/.vim
+    else
+        rm -f $HOME/.vim.old
+        mv $HOME/.vim $HOME/.vim.old
+        mkdir -p $HOME/.vim
     fi
+
+    ln -s $SCRIPT_PATH/$VIM_PATH/vimrc $HOME/.vim/vimrc
+    #
+    if [[ $DEV_ENABLE == "true" ]]; then
+        ln -s $SCRIPT_PATH/$VIM_PATH/plugins.vim $HOME/.vim/plugins.vim
+        ln -s $SCRIPT_PATH/$VIM_PATH/syntax $HOME/.vim/syntax
+    fi
+
     if [ -f "$HOME/.vimrc" ]; then
         rm -f $HOME/.vimrc.old
         mv $HOME/.vimrc $HOME/.vimrc.old
@@ -50,10 +54,23 @@ function install_vim {
         rm -f $HOME/.vimrc
         ln -s $HOME/.vim/vimrc $HOME/.vimrc
     fi
-
 }
 
+#function install_byobu_color {
+#    # install aliases
+#    if [ -f "$HOME/.byobu" ]; then
+#        rm -f $HOME/.aliases.old
+#        mv $HOME/.aliases $HOME/.aliases.old
+#        ln -s $SCRIPT_PATH/.aliases $HOME/.aliases
+#    else
+#        rm -f $HOME/.aliases
+#        ln -s $SCRIPT_PATH/.aliases $HOME/.aliases
+#    fi
+#
+#}
+
 function install_aliases {
+    echo "Installing aliases"
     # install aliases
     if [ -f "$HOME/.aliases" ]; then
         rm -f $HOME/.aliases.old
@@ -67,6 +84,7 @@ function install_aliases {
 }
 
 function install_zsh {
+    echo "Installing zshrc"
     # install zshrc
     if [[ $DEV_ENABLE == "true" ]]; then
         ZSH_PATH=".oh-my-zsh"
